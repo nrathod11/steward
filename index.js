b9c48af71e7ca5936a8f3b79973d0be91bb56f1e
Niyati Rathod <46799113+nrathod11@users.noreply.github.com>
2020-12-23 21:30:13 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
-'use strict';
+const config = require('config');
+const cors = require("cors");
+const morgan = require('morgan');
+const helmet = require('helmet');
+const express = require('express');
+const bodyParser = require('body-parser');
+//User route
+const users = require('./routes/users');
+//Hotel routes
+const hotels = require('./routes/hotels');
+//UserType routes
+const userTypes = require('./routes/usersTypes');
+// Category routes
+const category = require('./routes/categories');
+// Menu Item routes
+const menuItems = require('./routes/menuItems');
+// Main Menu routes
+const mainMenu = require('./routes/mainMenu');
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const log = require('./logger');
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+//setting up server using express
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(helmet());
+
+//Configuration
+console.log('Application Name:'+ config.get('name'));
+console.log('Mail Server'+ config.get('mail.host'));
+
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny')); 
+    console.log("Morgan enabled...")   
 }
+app.use(log);
+// Users route
+app.use('/api/users',users); 
+app.use('/api/hotels',hotels); 
+app.use('/api/usertypes',userTypes);
+app.use('/api/categories',category);
+app.use('/api/menuitems',menuItems);
+app.use('/api/getmenu',mainMenu);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+// PORT on which server is running
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running.. on Port ${PORT}`);
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
-module.exports = db;
+
